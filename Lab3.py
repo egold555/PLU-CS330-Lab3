d7771f9f671db4cb6e36200237f441905bad4110
Eric Golde <eric@golde.org>
2023-10-06 10:17:25 -0700

    Lab 3 with documentation and a new gitignore

--- Lab3.py
+++ Lab3.py
@@ -1,10 +1,10 @@
 """
- Name: Your Name
+ Name: Eric Golde
  Assignment: Lab 3 - Process dataset
  Course: CS 330
- Semester: 20xx
+ Semester: 2023
  Instructor: Dr. Cao
- Date: the current date
+ Date: 10/5/2023
  Sources consulted: any books, individuals, etc consulted
 
  Known Bugs: description of known bugs and other program imperfections
@@ -17,6 +17,8 @@
 import sys
 import argparse
 import math
+import random
+
 
 def splitData(data, trainData, testData, ratio):
     """
@@ -30,7 +32,13 @@ def splitData(data, trainData, testData, ratio):
             There is no grading script for this function, because different group may select different dataset depending on their course project, but generally you should make sure that you code can divide the dataset correctly, since you may use it for the course project
     """
     # your code here
-    pass
+
+    for d in data:
+        if len(trainData) < len(data) * ratio:
+            trainData.append(d)
+        else:
+            testData.append(d)
+
 
 def splitDataRandom(data, trainData, testData, ratio):
     """
@@ -44,17 +52,62 @@ def splitDataRandom(data, trainData, testData, ratio):
             Almost same as splitData, the only difference is this function will randomly shuffle the input data, so you will randomly select data and store it in the trainData
     """
     # your code here
+    random.shuffle(data)
+    splitData(data, trainData, testData, ratio)
     pass
+
 
 def main():
     options = parser.parse_args()
-    mode = options.mode       # first get the mode
+    mode = options.mode  # first get the mode
     print("mode is " + mode)
     """
     similar to Lab 2, please add your testing code here
     """
     # your code here
-    pass
+
+    inputFile = options.input
+
+    ratio = float(options.ratio)
+
+    skipNLines = int(options.skiplines)
+
+    inputData = []
+    trainData = []
+    testData = []
+
+    # read in the data
+    with open(inputFile, 'r') as f:
+        for line in f:
+            inputData.append(line.strip())
+
+    # skip the first n lines
+    if skipNLines > 0:
+        inputData = inputData[skipNLines:]
+
+    # split the data
+    if mode == 'R':
+        print("Splitting random with ratio " + str(ratio) + "...")
+        splitDataRandom(inputData, trainData, testData, ratio)
+    elif mode == 'N':
+        print("Splitting normal with ratio " + str(ratio) + "...")
+        splitData(inputData, trainData, testData, ratio)
+    else:
+        print("Invalid mode")
+        sys.exit(0)
+
+
+    # write the data to the files
+    with open("trainData.txt", 'w') as f:
+        for line in trainData:
+            f.write(line + "\n")
+
+    with open("testData.txt", 'w') as f:
+        for line in testData:
+            f.write(line + "\n")
+
+    print("Finished!")
+
 
 def showHelper():
     """
@@ -67,19 +120,31 @@ def showHelper():
 
 
 if __name__ == "__main__":
-    #------------------------arguments------------------------------#
-    #Shows help to the users                                        #
-    #---------------------------------------------------------------#
+    # ------------------------arguments------------------------------#
+    # Shows help to the users                                        #
+    # ---------------------------------------------------------------#
     parser = argparse.ArgumentParser()
     parser._optionals.title = "Arguments"
     parser.add_argument('--mode', dest='mode',
-    default = '',    # default empty!
-    help = 'Mode: R for random splitting, and N for the normal splitting')
+                        default='R',  # default empty!
+                        help='Mode: R for random splitting, and N for the normal splitting')
+
+    parser.add_argument('--ratio', dest='ratio',
+                        default='0.5',
+                        help='Ratio: the percentage of splitting. Decimal between 0 and 1')
+
+    parser.add_argument('--skiplines', dest='skiplines',
+                        default='0',
+                        help='Skip n lines: the number of lines to skip in the input file')
+
+    parser.add_argument('--input', dest='input',
+                        default='',  # default empty!
+                        help='Input file name')
 
     """
     Similar to Lab 2, please update the argument, and add as you need
     """
     # your code here
-    if len(sys.argv)<3:
+    if len(sys.argv) < 3:
         showHelper()
     main()
--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # Lab3
-This is my readme file!
+This program splits input data into training and test data.
 
-In here it would list all the contributors. Right now it's just me in my little lonely world.
+With the arguments below, you can randomize the data, skip lines, and provide a ratio of training data to test data.
 
 ## Contributors
 - [x] Eric Golde
 
+
+## How to use the program
+
+### Arguments
+| Argument | Type | Description | Defaults
+| --- | --- | --- | --- |
+| --help | N/A | Help menu | N/A |
+| --input | String | Input file | N/A - Required |
+| --skiplines | Integer | Amount of lines to skip from the top | 0 |
+| --ratio | Float | Ratio of training data to test data. 0-1 | 0.5 |
+| --mode | Char | Mode of the program. **R**andom or **N**ormal | R |
+
+### Example
+```bash
+# This will run the program, evenly splitting the data into training and test data, and skipping the CSV header, and randomly selecting the data
+python3 Lab3.py --input "DATA/covid19_open_data.csv" --skiplines 1 --ratio 0.5 --mode R
+```
